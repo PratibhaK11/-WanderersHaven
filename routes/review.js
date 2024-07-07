c65ec20eb63d59e81b68298c821bf1cb43975946
PratibhaK11 <kpratibha1620@gmail.com>
2024-07-07 14:48:01 +0530

    Refactor review operations into reviewController Created reviewController to handle CRUD operations for reviews. Organized review-related callback functions into createReview and destroyReview methods.

--- routes/review.js
+++ routes/review.js
@@ -6,34 +6,16 @@ const Review = require("../models/reviews.js");
 const Listing = require("../models/listing.js");
 const {validateReview, isLoggedIn, isReviewAuthor} = require("../middleware.js");
 
-
+const reviewController = require("../controllers/reviews.js");
 
 
 //Post review route
 router.post("/", isLoggedIn,
-    validateReview, wrapAsync(async (req, res) => {
-    let listing = await Listing.findById(req.params.id);
-    let newReview = new Review(req.body.review);
-    newReview.author = req.user._id;
-    
-    listing.reviews.push(newReview);
-    await newReview.save();
-    await listing.save();
-    req.flash("success", "New review added!");
-    res.redirect(`/listing/${listing._id}`);
-}));
+    validateReview, wrapAsync(reviewController.createReview));
 
 //delete review route
 router.delete("/:reviewId", isLoggedIn, isReviewAuthor, 
-    wrapAsync(async (req, res) => {
-
-    let { id, reviewId } = req.params;
-
-    await Listing.findByIdAndUpdate(id, {$pull: {reviews: reviewId}});
-    await Review.findByIdAndDelete(reviewId);
-    req.flash("success", "Review Deleted!");
-    res.redirect(`/listing/${id}`);
-}));
+    wrapAsync(reviewController.destroyReview));
 
 module.exports = router;
 
