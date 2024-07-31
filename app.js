047a9738da2ff7e2f547862a14e87914a40a9043
Pratibha Kumari <kpratibha1620@gmail.com>
2024-08-01 02:32:10 +0530

    minor fixes

--- app.js
+++ app.js
@@ -69,7 +69,7 @@ passport.deserializeUser(User.deserializeUser());
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "https://wanderershaven.onrender.com/auth/google/callback"
+    callbackURL: "/auth/google/callback"
 },
 async (accessToken, refreshToken, profile, done) => {
     try {
