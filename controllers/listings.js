928f2b3c720fbad4d62b1bbed43b77256c273879
Pratibha Kumari <kpratibha1620@gmail.com>
2024-08-01 18:09:52 +0530

    Added listing categories

--- controllers/listings.js
+++ controllers/listings.js
@@ -7,7 +7,6 @@ module.exports.index = async (req, res) => {
     const allListing = await Listing.find({});
     res.render("listings/index.ejs", { allListing });
 };
-
 exports.showTrendingListings = async (req, res) => {
     console.log('Trending route hit');
     try {
@@ -44,42 +43,28 @@ module.exports.showListing = async (req, res) => {
     res.render("listings/show.ejs", { listing });
 };
 
+
 module.exports.createListing = async (req, res) => {
-    console.log('Request Body:', req.body); // Log the entire request body
-    console.log('Category:', req.body.listing.category); // Log the category field
+    let response = await geocodingClient.forwardGeocode({
+        query: req.body.listing.location,
+        limit: 1,
+      })
+        .send()
+    
+    // Extracting file information from req.file
+    const { path: url, filename } = req.file;
 
-    try {
-        // Geocode the location
-        let response = await geocodingClient.forwardGeocode({
-            query: req.body.listing.location,
-            limit: 1,
-        }).send();
+    const newListing = new Listing(req.body.listing);
+    newListing.owner = req.user._id;
+    newListing.image = { url, filename };
+    newListing.image = { url, filename };
+    
+    newListing.geometry = response.body.features[0].geometry;
 
-        // Extract the image file information
-        const { path: url, filename } = req.file;
-
-        // Create a new listing object
-        const newListing = new Listing(req.body.listing);
-        newListing.owner = req.user._id;
-        newListing.image = { url, filename };
-        newListing.geometry = response.body.features[0].geometry;
-
-        // Ensure category is an array (handles single/multiple category cases)
-        if (!Array.isArray(newListing.category)) {
-            newListing.category = [newListing.category];
-        }
-
-        // Save the new listing to the database
-        let savedListing = await newListing.save();
-        console.log(savedListing);
-        req.flash("success", "New Listing Created!");
-        res.redirect("/listing");
-
-    } catch (err) {
-        console.error("Error creating listing:", err);
-        req.flash("error", "Failed to create listing.");
-        res.redirect("/listing/new");
-    }
+    let savedListing = await newListing.save();
+    console.log(savedListing);
+    req.flash("success", "New Listing Created!");
+    res.redirect("/listing");
 };
 
 
@@ -93,12 +78,14 @@ module.exports.renderEditForm = async (req, res) => {
     let originalImageUrl = listing.image.url;
     originalImageUrl = originalImageUrl.replace("/upload", "/upload/w_250");
     res.render("listings/edit.ejs", { listing, originalImageUrl });
+
 };
 
 module.exports.updateListing = async (req, res) => {
     let { id } = req.params;
     let listing = await Listing.findByIdAndUpdate(id, { ...req.body.listing });
     if (typeof req.file !== "undefined") {
+        // Extracting file information from req.file
         let { path: url, filename } = req.file;
         listing.image = { url, filename };
         await listing.save();
@@ -108,14 +95,9 @@ module.exports.updateListing = async (req, res) => {
 };
 
 module.exports.destroyListing = async (req, res) => {
-    try {
-        let { id } = req.params;
-        await Listing.findByIdAndDelete(id);
-        req.flash("success", "Listing Deleted!");
-        res.redirect("/listing");
-    } catch (err) {
-        console.error("Error deleting listing:", err);
-        req.flash("error", "Failed to delete listing.");
-        res.redirect("/listing");
-    }
+    let { id } = req.params;
+    await Listing.findByIdAndDelete(id);
+    req.flash("success", "Listing Deleted!");
+    res.redirect("/listing");
 }
+
