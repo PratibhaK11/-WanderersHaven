eefb305758c863252eb0804ca391f7553701634a
Praibha Kumari <kpratibha1620@gmail.com>
2024-08-05 05:29:16 +0530

    listing model

--- models/listing.js
+++ models/listing.js
@@ -1,46 +1,3 @@
-// const mongoose = require("mongoose");
-// const Schema = mongoose.Schema;
-// const Review = require("./reviews.js");
-
-// const listingSchema = new Schema({
-//   title: {
-//     type: String,
-//     required: true,
-//   },
-//   description: String,
-//   image: {
-//     url: String,
-//     filename: String,
-// },
-//   price: Number,
-//   location: String,
-//   country: String,
-//   reviews: [
-//     {
-//         type: Schema.Types.ObjectId,
-//         ref: "Review"
-//     }
-//   ],
-
-//   owner: {
-//     type: Schema.Types.ObjectId,
-//     ref: "User"
-//   },
-
-// });
-
-// listingSchema.post("findOneAndDelete", async (listing) => {
-//   if(listing){
-//     await Review.deleteMany({_id: {$in: listing.reviews}});
-//   }
-
-// });
-
-// const Listing = mongoose.model("Listing", listingSchema);
-// module.exports = Listing;
-
-
-
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const Review = require("./reviews.js");
@@ -74,24 +31,30 @@ const listingSchema = new Schema({
     type: {
       type: String,
       enum: ['Point'],
-      
+      required: true
     },
     coordinates: {
       type: [Number],
-     
+      required: true
     }
   },
   bookingCount: {
     type: Number,
     default: 0
   },
- 
   category: {
     type: [String],
     enum: ['Castles', 'Mountains', 'Iconic Cities', 'Room', 'Boats', 'Arctic', 'Farms', 'Camping', 'Amazing Pools'],
     required: true
   }
-
 });
+
+// Post hook to delete related reviews when a listing is deleted
+listingSchema.post("findOneAndDelete", async function(listing) {
+  if (listing) {
+    await Review.deleteMany({ _id: { $in: listing.reviews } });
+  }
+});
+
 const Listing = mongoose.model("Listing", listingSchema);
 module.exports = Listing;
