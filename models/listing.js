cf14004a02e46590fb32aabf66bbe9192fd02897
Praibha Kumari <kpratibha1620@gmail.com>
2024-08-05 06:55:54 +0530

    listing model

--- models/listing.js
+++ models/listing.js
@@ -44,7 +44,7 @@ const listingSchema = new Schema({
   },
   category: {
     type: [String],
-    enum: ['Castles', 'Mountains', 'Iconic Cities', 'Room', 'Boats', 'Arctic', 'Farms', 'Camping', 'Amazing Pools'],
+    // enum: ['Castles', 'Mountains', 'Iconic Cities', 'Room', 'Boats', 'Arctic', 'Farms', 'Camping', 'Amazing Pools'],
     required: true,
     default: [] // Default to an empty array if not provided
 }
